8f670155353357dd6d88a307d7a8670c97b27bf1
balashankar1180 <balashankar1180@gmail.com>
2021-06-20 14:37:39 +0530

    2 files modified, 3 added

---
+++ src/app/simple/simple.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simple',
+  templateUrl: './simple.component.html',
+  styleUrls: ['./simple.component.css']
+})
+export class SimpleComponent {
+userText: string = "balu";
+}
